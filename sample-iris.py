b39d952dcba846c2850815b06a4393e8f420a715
CWH91 <109711093+CWH91@users.noreply.github.com>
2022-07-21 15:27:08 +0800

    Update sample-iris.py

--- sample-iris.py
+++ sample-iris.py
@@ -29,7 +29,7 @@ st.write(df)
 
 iris = pd.read_csv('https://raw.githubusercontent.com/CWH91/iris-july/main/IRIS.csv')
 X = iris.drop('species',axis=1)
-Y = iris.['species']
+Y = iris['species']
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
